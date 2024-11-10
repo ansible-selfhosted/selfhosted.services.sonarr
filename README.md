d349474933dfc5ce356d7da0000ca454cfd5a6b2
SpeakinTelnet <gui.lac@protonmail.com>
2024-11-10 10:40:48 -0500

    Initial commit from template

---
+++ README.md
@@ -0,0 +1,12 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+[![Copier](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/copier-org/copier/master/img/badge/badge-grayscale-inverted-border.json)](https://github.com/copier-org/copier)
+[![Podman Badge](https://img.shields.io/badge/Podman-892CA0?logo=podman&logoColor=white)](https://podman.io/)
+[![Hatch project](https://img.shields.io/badge/%F0%9F%A5%9A-Hatch-4051b5.svg)](https://github.com/pypa/hatch)
+![CI](https://github.com/ansible-selfhosted/selfhosted.services.sonarr.git/actions/workflows/ci.yml/badge.svg)
+[![Ansible](https://img.shields.io/badge/Ansible-Molecule-EE0000?style=plastic&logo=ansible&logoColor=white)](https://github.com/ansible/molecule)
+
+<!-- BEGIN_ANSIBLE_DOCS -->
+
+### The readme content is auto-generated with the `hatch run docs:generate` command.
+
+<!-- END_ANSIBLE_DOCS -->
---
+++ generate_readme.py
@@ -0,0 +1,103 @@
+import pathlib
+from ruamel.yaml import YAML, YAMLError
+
+yaml = YAML()
+
+meta_path = pathlib.Path(__file__).parent / "meta" / "main.yml"
+arguments_path = pathlib.Path(__file__).parent / "meta" / "argument_specs.yml"
+extra_data_path = pathlib.Path(__file__).parent / "meta" / "extra_data.yml"
+readme_path = pathlib.Path(__file__).parent / "README.md"
+
+
+def parse_yaml(path):
+    data = {}
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            data = yaml.load(f)
+    except YAMLError:
+        print(f"Failed to parse {path}")
+    return data
+
+
+def make_text(meta, arguments, extras):
+    has_choices = any("choices" in v for v in arguments["options"].values())
+    nl = "\n"  # workaround for f-string backslash limitation
+    try:
+        text = f"""
+# Ansible Role: {(
+    f"[{meta['galaxy_info']['role_name']}]({extras['related_url']})" 
+    if extras['related_url'] 
+    else f"{meta['galaxy_info']['role_name']}"
+    )}
+
+{meta['galaxy_info']['description']}
+
+## Role Requirements
+
+{(
+    "- none" 
+    if len(extras['requirements']) == 0 
+    else 
+    nl.join(f"- {r}" for r in extras['requirements'])
+  )}
+
+*Refer to {extras['collection']} collection for general requirements*
+
+## Role Arguments
+
+|Option|Description|Type|Required|Default|{'choices|' if has_choices else ''}
+|---|---|---|---|---|{'---|' if has_choices else ''}
+{(
+    "".join((f"|{k}"
+             f"|{"<br>".join(v['description'])}|{v['type']}"
+             f"|{v['required']}"
+             f"|{v['default']}"
+             f"|{("<br>".join(
+                f"- {c}" for c in v['choices'] + "|")
+                if 'choices' in v
+                else '')}"
+             f"{nl}")
+        for k, v in arguments['options'].items())
+    )}
+
+## Example Playbook
+
+```
+- hosts: all
+  tasks:
+    - name: Importing {meta['galaxy_info']['role_name']} role
+      ansible.builtin.import_role:
+        name: selfhosted.{extras['collection']}.{meta['galaxy_info']['role_name']}
+      vars:
+```
+
+## License
+
+This project is licensed under the [{meta['galaxy_info']['license']} License](LICENSE)
+
+
+⊂(▀¯▀⊂)
+
+"""
+    except KeyError as e:
+        raise KeyError(f"key {e} not found, cannot create README.md")
+
+    return text
+
+
+def inject_to_readme(text, path):
+    with open(path, "r") as f:
+        lines = f.readlines()
+    startline = lines.index("<!-- BEGIN_ANSIBLE_DOCS -->\n")
+    endline = lines.index("<!-- END_ANSIBLE_DOCS -->\n")
+    new_text = "".join(lines[: startline + 1]) + text + "".join(lines[endline:])
+    with open(path, "w") as f:
+        f.write(new_text)
+
+
+if __name__ == "__main__":
+    meta = parse_yaml(meta_path)
+    arguments = parse_yaml(arguments_path)
+    extras = parse_yaml(extra_data_path)
+    text = make_text(meta, arguments["argument_specs"]["main"], extras)
+    inject_to_readme(text, readme_path)
